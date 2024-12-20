9775f838b1f47e984407e62361e7e91246c24a19
gabrielohdez <hbjosegabriel@gmail.com>
2024-12-20 00:05:10 -0600

    mejoras

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,7 +5,7 @@ function Message({ text, sender }) {
   return (
     <div className={`message ${sender}`}>
       <div className="message-icon">
-        {sender === 'user' ? <FaUser /> : <VscRobot />}
+        {sender === 'user' ? <FaUser /> : <FaRobot />}
       </div>
       <p>{text}</p>
     </div>
