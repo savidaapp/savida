a27e2cf2eab6e0dfc0e76ffe2c1b0849549e3866
gabrielohdez <hbjosegabriel@gmail.com>
2024-12-19 23:56:34 -0600

    mejoras

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { VscRobot } from 'react-icons/vsc';
+import { VscRobot } from 'react-icons/vsc'; // Correcto
 import { FaUser } from 'react-icons/fa';
 
 function Message({ text, sender }) {
   return (
     <div className={`message ${sender}`}>
       <div className="message-icon">
-        {sender === 'user' ? <FaUser /> : <VscRobot />}
+        {sender === 'user' ? <FaUser /> : <VscRobot />} {/* Icono de robot */}
       </div>
       <p>{text}</p>
     </div>
